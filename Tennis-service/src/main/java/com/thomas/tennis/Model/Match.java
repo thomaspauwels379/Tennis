c2b3825b2af1abcfb76e2d513a726d010283d9ec
thomaspauwels379 <thomas_pauwels@live.com>
2026-02-09 14:41:15 +0100

    added match model

---
+++ Tennis-service/src/main/java/com/thomas/tennis/Model/Match.java
@@ -0,0 +1,39 @@
+package com.thomas.tennis.Model;
+
+import com.thomas.tennis.Enums.MatchState;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Match {
+
+    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "player", referencedColumnName = "id")
+    private Player player1;
+
+    @OneToOne
+    @JoinColumn(name = "player", referencedColumnName = "id")
+    private Player player2;
+
+    private MatchState state;
+
+    protected Match(){}
+
+    public Match(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.state = MatchState.STARTED;
+    }
+}
--- Tennis-service/src/main/java/com/thomas/tennis/Model/Player.java
+++ Tennis-service/src/main/java/com/thomas/tennis/Model/Player.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Entity
