b0830b2bf4a0cc047d2759f564fd80a350866b26
thomaspauwels379 <thomas_pauwels@live.com>
2026-02-13 14:45:55 +0100

    removed comments

--- my-tennis-app/components/GameEngine.tsx
+++ my-tennis-app/components/GameEngine.tsx
@@ -10,17 +10,12 @@ export default function GameEngine() {
     if (!canvas) return;
     const ctx = canvas.getContext('2d');
     if (!ctx) return;
-
-    // Hier komt later je teken-logica (ctx.draw...)
-    
   }, []);
 
   return (
     <canvas 
       ref={canvasRef}
       className="w-full h-full"
-      // We geven een standaard resolutie mee, 
-      // de CSS (w-full/h-full) zorgt voor de schaling
       width={1600} 
       height={900}
     />
